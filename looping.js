49676d59c6d973d2e52912324a4728b7bcebcb33
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-26 21:58:07 +0600

    first commit

---
+++ looping.js
@@ -0,0 +1,17 @@
+const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+for(const friend of friends){
+    console.log(friend);
+}
+
+
+for(let i=0; i<friends.length; i++){
+    console.log(i);
+    console.log(friends[i]);
+}
+
+let j=0;
+while(j<friends.length){
+    console.log(friends[j]);
+    j++;
+}
+
